07fd1b9ea29b324412e800b6c7dbf6cca34f29a7
Keisuke Kishimoto <keisuke@google.com>
2020-05-18 00:52:05 +0200

    make tests read nicer

--- modint_test.cc
+++ modint_test.cc
@@ -10,61 +10,58 @@ TEST(modint, default_construct) {
 }
 
 TEST(modint, construct) {
-  ModInt<7> m(10);
+  ModInt<7> m = 10;
   EXPECT_EQ(m, 3);
 }
 
 TEST(modint, add) {
-  ModInt<7> m(2);
+  ModInt<7> m = 2;
   m += 6;
   EXPECT_EQ(m, 1);
 
-  ModInt<11> a(7);
-  ModInt<11> b(8);
+  ModInt<11> a = 7, b = 8;
   EXPECT_EQ(a + b, 4);
 }
 
 TEST(modint, increment) {
-  ModInt<7> m(6);
+  ModInt<7> m = 6;
   ++m;
   EXPECT_EQ(m, 0);
 }
 
 TEST(modint, sub) {
-  ModInt<7> m(2);
+  ModInt<7> m = 2;
   m -= 6;
   EXPECT_EQ(m, 3);
 
-  ModInt<11> a(7);
-  ModInt<11> b(8);
+  ModInt<11> a = 7, b = 8;
   EXPECT_EQ(a - b, 10);
 }
 
 TEST(modint, decrement) {
-  ModInt<7> m(0);
+  ModInt<7> m;
   --m;
   EXPECT_EQ(m, 6);
 }
 
 TEST(modint, mul) {
-  ModInt<7> m(2);
+  ModInt<7> m = 2;
   m *= 6;
   EXPECT_EQ(m, 5);
 
-  ModInt<11> a(7);
-  ModInt<11> b(8);
+  ModInt<11> a = 7, b = 8;
   EXPECT_EQ(a * b, 1);
 }
 
 TEST(modint, large) {
-  ModInt<1000000007> a(2147483647), b(2147483646);
+  ModInt<1000000007> a = 2147483647, b = 2147483646;
   EXPECT_EQ(a + b, 294967265);
   EXPECT_EQ(a * b, 703135109);
 }
 
 TEST(modint, stream) {
   std::ostringstream oss;
-  ModInt<7> m(2);
+  ModInt<7> m = 2;
   oss << m;
   EXPECT_EQ(oss.str(), "2");
 }
