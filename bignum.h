641532de97692fb14b75e210be3abacece1f4bd7
Keisuke Kishimoto <keisuke@google.com>
2020-06-03 11:53:58 +0200

    add decfloat

--- bignum.h
+++ bignum.h
@@ -1,5 +1,7 @@
 // Prerequisite:
 // $ sudo apt-get install libboost-all-dev
+#include <boost/multiprecision/cpp_dec_float.hpp>
 #include <boost/multiprecision/cpp_int.hpp>
 
 using bigint = boost::multiprecision::cpp_int;
+using decfloat = boost::multiprecision::cpp_dec_float_50;
--- bignum_test.cc
+++ bignum_test.cc
@@ -2,8 +2,16 @@
 
 #include "gtest/gtest.h"
 
-TEST(bigint, simple) {
+TEST(bignum, bigint) {
   bigint a = 1LL << 60;
   bigint b = a * a;
   EXPECT_EQ(b.str(), "1329227995784915872903807060280344576");
+}
+
+TEST(bignum, decfloat) {
+  decfloat a("2.51");
+  EXPECT_EQ(int(a * 100), 251);
+
+  double b = 2.51;
+  EXPECT_EQ(int(b * 100), 250);
 }
