009c706480314982c5a56c18b4af24d1845ecc72
Keisuke Kishimoto <keisuke@google.com>
2020-06-07 17:58:55 +0200

    prevent overflow

--- binary_search.h
+++ binary_search.h
@@ -3,7 +3,7 @@
 template <typename T>
 T BinarySearch(T lo, T hi, std::function<bool(T)> pred) {
   while (lo + 1 < hi) {
-    T mid = (lo + hi) / 2;
+    T mid = (hi - lo) / 2 + lo;
     if (pred(mid)) {
       lo = mid;
     } else {
