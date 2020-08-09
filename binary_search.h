58084c173d12af0a54fbbe282ad7f100e0f677ea
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-09 20:21:00 +0200

    update the binary search interface

--- binary_search.h
+++ binary_search.h
@@ -1,14 +1,10 @@
 #include <functional>
 
 template <typename T>
-T BinarySearch(T lo, T hi, std::function<bool(T)> pred) {
-  while (lo + 1 < hi) {
-    T mid = (hi - lo) / 2 + lo;
-    if (pred(mid)) {
-      lo = mid;
-    } else {
-      hi = mid;
-    }
+T BinarySearch(T ok, T ng, std::function<bool(T)> pred) {
+  while (abs(ok - ng) > 1) {
+    T mid = (ok + ng) / 2;
+    (pred(mid) ? ok : ng) = mid;
   }
-  return lo;
+  return ok;
 }
