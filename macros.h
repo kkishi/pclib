7cf9735cbafc703eddd9d7f06fa26e50e547e6e4
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-16 17:03:57 +0200

    update

--- macros.h
+++ macros.h
@@ -10,13 +10,12 @@ template <typename T>
 struct is_container<T, std::void_t<decltype(std::declval<T>().begin()),
                                    decltype(std::declval<T>().end())>>
     : std::true_type {};
-
 template <typename T, typename _ = void>
 struct is_pair : std::false_type {};
 template <typename T>
-struct is_pair<T, std::void_t<typename T::first_type, typename T::second_type>>
+struct is_pair<T, std::void_t<decltype(std::declval<T>().first),
+                              decltype(std::declval<T>().second)>>
     : std::true_type {};
-
 template <typename T>
 void debug(const T& v) {
   if constexpr (is_pair<T>::value) {
