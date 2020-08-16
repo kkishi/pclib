47bfbb9f34ee51cb85c5359fb7ae305cd060d6ff
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-16 15:51:19 +0200

    simplify

--- macros.h
+++ macros.h
@@ -6,17 +6,10 @@
 
 template <typename T, typename _ = void>
 struct is_container : std::false_type {};
-
-template <typename... Ts>
-struct is_container_helper {};
-
 template <typename T>
-struct is_container<
-    T,
-    std::conditional_t<false,
-                       is_container_helper<decltype(std::declval<T>().begin()),
-                                           decltype(std::declval<T>().end())>,
-                       void>> : public std::true_type {};
+struct is_container<T, std::void_t<decltype(std::declval<T>().begin()),
+                                   decltype(std::declval<T>().end())>>
+    : std::true_type {};
 
 template <typename T,
           typename std::enable_if<!is_container<T>::value>::type* = nullptr>
