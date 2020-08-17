ba9f99a7ac90540ffab3faae9c6b4b7d36de42bc
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-08-17 03:12:22 +0200

    fix binary search overflow

--- binary_search.h
+++ binary_search.h
@@ -2,8 +2,9 @@
 
 template <typename T>
 T BinarySearch(T ok, T ng, std::function<bool(T)> pred) {
-  while (abs(ok - ng) > 1) {
-    T mid = (ok + ng) / 2;
+  static_assert(std::is_integral<T>::value);
+  while ((ok > ng ? ok - ng : ng - ok) > 1) {
+    T mid = ok + (ng - ok) / 2;
     (pred(mid) ? ok : ng) = mid;
   }
   return ok;
--- binary_search_test.cc
+++ binary_search_test.cc
@@ -1,5 +1,6 @@
 #include "binary_search.h"
 
+#include <limits>
 #include <vector>
 
 #include "gtest/gtest.h"
@@ -8,4 +9,11 @@ TEST(binary_search, find) {
   std::vector<int> v = {-1, 3, 5, 7, 13};
   int i = BinarySearch<int>(0, v.size(), [&](int i) { return v[i] <= 5; });
   EXPECT_EQ(i, 2);
+}
+
+TEST(binary_search, large) {
+  using ll = long long;
+  ll m = std::numeric_limits<ll>::max();
+  ll result = BinarySearch<ll>(0, m, [&](ll x) { return x <= m - 42; });
+  EXPECT_EQ(result, m - 42);
 }
