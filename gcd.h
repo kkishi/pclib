a54c68615ca62f127dc84a169d406aa450992f8b
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2020-12-20 15:05:13 +0100

    Add ExtGcd

--- gcd.h
+++ gcd.h
@@ -1,7 +1,20 @@
+#include <utility>
+
 template <typename T>
 T Gcd(T a, T b) {
   if (b == 0) {
     return a;
   }
   return Gcd(b, a % b);
+}
+
+// auto [g, x, y] = ExtGcd(a, b);
+// => a * x + b * y = g = gcd(a, b)
+template <typename T>
+std::tuple<T, T, T> ExtGcd(T a, T b) {
+  if (b == 0) {
+    return {a, 1, 0};
+  }
+  auto [g, x, y] = ExtGcd(b, a % b);
+  return {g, y, x - (a / b) * y};
 }
--- gcd_test.cc
+++ gcd_test.cc
@@ -8,3 +8,9 @@ TEST(gcd, simple) {
   EXPECT_EQ(Gcd<int>(20, 100), 20);
   EXPECT_EQ(Gcd<int>(624129, 2061517), 18913);
 }
+
+TEST(extgcd, simple) {
+  EXPECT_EQ(ExtGcd<int>(252, 105), std::make_tuple(21, -2, 5));
+  EXPECT_EQ(ExtGcd<int>(111, 30), std::make_tuple(3, 3, -11));
+  EXPECT_EQ(ExtGcd<int>(624129, 2061517), std::make_tuple(18913, -33, 10));
+}
