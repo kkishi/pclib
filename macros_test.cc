2e5aed2cc9a4cfd654f17cf4df06ea8495a9f2ad
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-01-01 13:06:48 +0100

    add a test for maybe_make_signed.

--- macros_test.cc
+++ macros_test.cc
@@ -5,12 +5,23 @@
 
 #include "gtest/gtest.h"
 
+using std::array;
+using std::is_same;
+using std::map;
+using std::numeric_limits;
+using std::optional;
+using std::pair;
+using std::set;
+using std::string;
+using std::tuple;
+using std::vector;
+
 TEST(macros, simple) {
-  std::set<int> s;
+  set<int> s;
   rep(i, 3) s.insert(i);
 
-  std::vector v(all(s));
-  EXPECT_EQ(v, std::vector({0, 1, 2}));
+  vector v(all(s));
+  EXPECT_EQ(v, vector({0, 1, 2}));
 }
 
 TEST(macros, V) {
@@ -23,22 +34,29 @@ TEST(macros, V) {
 TEST(macros, sfinae) {
   // Raw pointers and optionals are dereferenceable.
   EXPECT_TRUE(is_dereferenceable<int*>::value);
-  EXPECT_TRUE(is_dereferenceable<std::optional<int>>::value);
+  EXPECT_TRUE(is_dereferenceable<optional<int>>::value);
   EXPECT_FALSE(is_dereferenceable<int>::value);
-  EXPECT_FALSE((is_dereferenceable<std::tuple<int, int>>::value));
+  EXPECT_FALSE((is_dereferenceable<tuple<int, int>>::value));
 
   // Container like things are iterable. One caveat is that string is iterable,
   // but we don't generally want to treat it as such.
-  EXPECT_TRUE(is_iterable<std::vector<int>>::value);
-  EXPECT_TRUE((is_iterable<std::array<int, 1000000>>::value));
-  EXPECT_FALSE((is_iterable<std::tuple<int, int, int>>::value));
-  EXPECT_FALSE(is_iterable<std::string>::value);
+  EXPECT_TRUE(is_iterable<vector<int>>::value);
+  EXPECT_TRUE((is_iterable<array<int, 1000000>>::value));
+  EXPECT_FALSE((is_iterable<tuple<int, int, int>>::value));
+  EXPECT_TRUE(is_iterable<string>::value);
 
   // Tuple like things are applicable (can work with std::apply).
-  EXPECT_TRUE((is_applicable<std::pair<int, int>>::value));
-  EXPECT_TRUE((is_applicable<std::tuple<int, int>>::value));
-  EXPECT_FALSE(is_applicable<std::vector<int>>::value);
-  EXPECT_TRUE((is_applicable<std::array<int, 1000000>>::value));
+  EXPECT_TRUE((is_applicable<pair<int, int>>::value));
+  EXPECT_TRUE((is_applicable<tuple<int, int>>::value));
+  EXPECT_FALSE(is_applicable<vector<int>>::value);
+  EXPECT_TRUE((is_applicable<array<int, 1000000>>::value));
+
+  // Converts known unsigned types to their corresponding signed types, but
+  // leaves unknown types as is.
+  EXPECT_TRUE((is_same<maybe_make_signed<uint>::type, int>::value));
+  EXPECT_TRUE((is_same<maybe_make_signed<int>::type, int>::value));
+  EXPECT_TRUE(
+      (is_same<maybe_make_signed<__int128_t>::type, __int128_t>::value));
 }
 
 TEST(macros, debug) {
@@ -50,32 +68,27 @@ TEST(macros, debug) {
   using std::string;
   using std::tuple;
   using std::vector;
-  // Just confirm that it doesn't cause compilation err.r
-  vector<int> v = {1, 2, 3};
-  debug(v);
-  vector<int> s = {1, 2, 3};
-  debug(s);
-  pair<int, double> p = {1, 2.5};
-  debug(p);
-  tuple<int, int, int> t = {4, 5, 6};
-  debug(t);
-  map<int, string> m = {{42, "foo"}};
-  debug(m);
-  array<int, 4> a = {42};
-  debug(a);
-  int* ip = &v[0];
-  debug(ip);
-  optional<int> oi;
-  debug(oi);
-  oi = 42;
-  debug(oi);
-  map<pair<set<int>, vector<pair<string, double>>>, array<tuple<string>, 1>> c;
-  debug(c);
-  debug(v, s, p, m, a, c);
+
+  // Just confirm that it doesn't cause compilation error.
+  debug(int{42});
+  debug((array<int, 4>{42}));
+  debug((map<int, string>{{42, "foo"}}));
+  debug(optional<int>{});
+  debug(optional<int>{42});
+  debug((pair<int, double>{1, 2.5}));
+  debug((set<int>{1, 2, 3}));
+  debug(string{"str"});
+  debug((tuple<int, int, int>{4, 5, 6}));
+  debug((vector<int>{1, 2, 3}));
+  int i = 42;
+  debug(&i);
+  debug((int*){nullptr});
+  debug((map<pair<set<int>, vector<pair<string, double>>>,
+             array<tuple<string>, 1>>{}));
 }
 
 TEST(macros, popcount) {
-  EXPECT_EQ(popcount(std::numeric_limits<int64_t>::max()), 63);
+  EXPECT_EQ(popcount(numeric_limits<int64_t>::max()), 63);
 }
 
 TEST(macros, hasbit) {
@@ -102,7 +115,7 @@ TEST(macros, rep) {
   {
     {
       int cnt = 0;
-      __int128_t a = std::numeric_limits<__int128_t>::max() - 1, b = a + 1;
+      __int128_t a = numeric_limits<__int128_t>::max() - 1, b = a + 1;
       rep(i, a, b)++ cnt;
       EXPECT_EQ(cnt, 1);
     }
@@ -134,13 +147,13 @@ TEST(macros, rrep) {
 
 TEST(macros, each) {
   {
-    std::vector<int> v{1, 2, 3};
+    vector<int> v{1, 2, 3};
     int sum = 0;
     each(vi, v) sum += vi;
     EXPECT_EQ(sum, 6);
   }
   {
-    std::map<int, int> m{{1, 2}, {3, 4}};
+    map<int, int> m{{1, 2}, {3, 4}};
     int sum = 0;
     each(k, v, m) sum += k * v;
     EXPECT_EQ(sum, 14);
