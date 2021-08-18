3b595fdb9830a056cfee43c8ac969e01f4db9f2b
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-08-18 12:56:04 +0200

    add comments

--- intervals.h
+++ intervals.h
@@ -18,18 +18,23 @@ std::ostream& operator<<(std::ostream& os, const Interval& i) {
 
 class Intervals : public std::set<Interval> {
  public:
+  // Inserts an interval [left, right) with vlaue. If it would result in
+  // consecutive intervals with the same value, they are concatenated.
+  //
+  // NOTE: The new interval must not overlap with any existing intervals. Call
+  // Erase first to ensure that if necessary.
   void Insert(int left, int right, int value = 0) {
     auto it = LowerBound(left);
     if (it != begin()) {
       auto pit = prev(it);
-      assert(pit->right <= left);
+      assert(pit->right <= left);  // Assert no overlap on the left.
       if (pit->right == left && pit->value == value) {
         left = pit->left;
         erase(pit);
       }
     }
     if (it != end()) {
-      assert(right <= it->left);
+      assert(right <= it->left);  // Assert no overlap on the right.
       if (right == it->left && value == it->value) {
         right = it->right;
         erase(it);
@@ -37,6 +42,9 @@ class Intervals : public std::set<Interval> {
     }
     insert({left, right, value});
   }
+
+  // Erases intervals that overlap with [left, right). Partial overlaps are
+  // erased partially. Returns erased intervals.
   std::vector<Interval> Erase(int left, int right) {
     auto it = LowerBound(left), jt = it;
     while (jt != end() && jt->left < right) ++jt;
@@ -54,16 +62,22 @@ class Intervals : public std::set<Interval> {
     }
     return erased;
   }
+
+  // Returns the first interval that either contains x or is on the right of x.
   std::set<Interval>::iterator LowerBound(int x) const {
     auto it = lower_bound({x, 0, 0});
     if (it != begin() && x < prev(it)->right) --it;
     return it;
   }
+
+  // Returns an interval containing x if exists.
   std::optional<Interval> Find(int x) const {
     auto it = LowerBound(x);
     if (it != end() && it->Contains(x)) return *it;
     return std::nullopt;
   }
+
+  // Returns mex[left, infinity).
   int Mex(int left) const {
     auto i = Find(left);
     return i ? i->right : left;
