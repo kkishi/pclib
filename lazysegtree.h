9a00d7a2962b29232f404145d4b7a43ec3fb5808
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-08-30 12:41:29 +0200

    add lazysegtree examples

--- lazysegtree.h
+++ lazysegtree.h
@@ -22,6 +22,38 @@ F composition(F f, F g) { return f + g; }
 using segtree = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
 }  // namespace add_min
 
+namespace update_sum {
+struct S {
+  int64_t val;
+  int64_t len;
+};
+using F = int64_t;
+S e() { return {0, 0}; }
+F id() { return std::numeric_limits<int>::max(); }
+S op(S a, S b) { return {a.val + b.val, a.len + b.len}; }
+S mapping(F f, S x) {
+  if (f != id()) x.val = f * x.len;
+  return x;
+}
+F composition(F f, F g) { return (f == id() ? g : f); }
+class segtree
+    : public atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> {
+ public:
+  segtree() : lazy_segtree() {}
+  explicit segtree(int n) : lazy_segtree(std::vector<S>(n, {0, 1})) {}
+  explicit segtree(const std::vector<int64_t>& v) : lazy_segtree(itos(v)) {}
+
+ private:
+  static std::vector<S> itos(const std::vector<int64_t>& v) {
+    std::vector<S> w(v.size());
+    for (size_t i = 0; i < v.size(); ++i) {
+      w[i] = {v[i], 1};
+    }
+    return w;
+  }
+};
+}  // namespace update_sum
+
 namespace add_sum {
 struct S {
   int64_t val;
@@ -33,5 +65,20 @@ F id() { return 0; }
 S op(S a, S b) { return {a.val + b.val, a.len + b.len}; }
 S mapping(F f, S x) { return {x.val + f * x.len, x.len}; }
 F composition(F f, F g) { return f + g; }
-using segtree = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
+class segtree
+    : public atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> {
+ public:
+  segtree() : lazy_segtree() {}
+  explicit segtree(int n) : lazy_segtree(std::vector<S>(n, {0, 1})) {}
+  explicit segtree(const std::vector<int64_t>& v) : lazy_segtree(itos(v)) {}
+
+ private:
+  static std::vector<S> itos(const std::vector<int64_t>& v) {
+    std::vector<S> w(v.size());
+    for (size_t i = 0; i < v.size(); ++i) {
+      w[i] = {v[i], 1};
+    }
+    return w;
+  }
+};
 }  // namespace add_sum
