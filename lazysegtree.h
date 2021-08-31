6b3ac42345b13ea3f184f475de965d73548e717f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-08-31 17:26:35 +0200

    use optional

--- lazysegtree.h
+++ lazysegtree.h
@@ -1,45 +1,42 @@
 #include <atcoder/lazysegtree>
+#include <optional>
 
-// NOTE: This implementation assumes that the value equal id() will never be
-// passed to the methods, such as apply. If it happens, it fails to update.
 namespace update_min {
 using S = int64_t;
-using F = int64_t;
-S e() { return std::numeric_limits<S>::max(); }
-F id() { return std::numeric_limits<F>::max(); }
 S op(S a, S b) { return std::min(a, b); }
-S mapping(F f, S x) { return f == id() ? x : f; }
-F composition(F f, F g) { return f == id() ? g : f; }
+S e() { return std::numeric_limits<S>::max(); }
+using F = std::optional<int64_t>;
+S mapping(F f, S x) { return f ? *f : x; }
+F composition(F f, F g) { return f ? f : g; }
+F id() { return std::nullopt; }
 using segtree = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
 }  // namespace update_min
 
 namespace add_min {
 using S = int64_t;
-using F = int64_t;
-S e() { return std::numeric_limits<S>::max(); }
-F id() { return 0; }
 S op(S a, S b) { return std::min(a, b); }
+S e() { return std::numeric_limits<S>::max(); }
+using F = int64_t;
 S mapping(F f, S x) { return f + x; }
 F composition(F f, F g) { return f + g; }
+F id() { return 0; }
 using segtree = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
 }  // namespace add_min
 
-// NOTE: This implementation assumes that the value equal id() will never be
-// passed to the methods, such as apply. If it happens, it fails to update.
 namespace update_sum {
 struct S {
   int64_t val;
   int64_t len;
 };
-using F = int64_t;
-S e() { return {0, 0}; }
-F id() { return std::numeric_limits<int>::max(); }
 S op(S a, S b) { return {a.val + b.val, a.len + b.len}; }
+S e() { return {0, 0}; }
+using F = std::optional<int64_t>;
 S mapping(F f, S x) {
-  if (f != id()) x.val = f * x.len;
+  if (f) x.val = *f * x.len;
   return x;
 }
-F composition(F f, F g) { return f == id() ? g : f; }
+F composition(F f, F g) { return f ? f : g; }
+F id() { return std::nullopt; }
 class segtree
     : public atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> {
  public:
@@ -63,12 +60,12 @@ struct S {
   int64_t val;
   int64_t len;
 };
-using F = int64_t;
-S e() { return {0, 0}; }
-F id() { return 0; }
 S op(S a, S b) { return {a.val + b.val, a.len + b.len}; }
+S e() { return {0, 0}; }
+using F = int64_t;
 S mapping(F f, S x) { return {x.val + f * x.len, x.len}; }
 F composition(F f, F g) { return f + g; }
+F id() { return 0; }
 class segtree
     : public atcoder::lazy_segtree<S, op, e, F, mapping, composition, id> {
  public:
