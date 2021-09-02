7148b0591b8a543e3d97655855d1269fa47d37c2
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-09-02 09:24:26 +0200

    fix a bug in z algorithm

--- z_algorithm.h
+++ z_algorithm.h
@@ -5,34 +5,33 @@
 // https://www.cs.cmu.edu/~ckingsf/bioinfo-lectures/zalg.pdf
 std::vector<int64_t> ZAlgorithm(const std::string& s) {
   int n = s.size();
-  std::vector<int64_t> Z(n);
-  Z[0] = n;
+  std::vector<int64_t> z(n);
+  z[0] = n;
   int l = 0, r = 0;
   for (int k = 1; k < n; ++k) {
     if (r <= k) {
       l = r = k;
       while (r < n && s[r - l] == s[r]) ++r;
-      Z[k] = r - l;
+      z[k] = r - l;
       continue;
     }
-    int Zkd = Z[k - l];
-    if (Zkd < r - k) {
-      Z[k] = Zkd;
+    if (z[k - l] < r - k) {
+      z[k] = z[k - l];
       continue;
     }
     l = k;
-    while (r < n && s[Zkd] == s[r]) ++Zkd, ++r;
-    Z[k] = r - l;
+    while (r < n && s[r - l] == s[r]) ++r;
+    z[k] = r - l;
   }
-  return Z;
+  return z;
 }
 
 std::vector<int> ZMatch(const std::string& t, const std::string& p) {
-  auto v = ZAlgorithm(p + '$' + t);
+  auto z = ZAlgorithm(p + '$' + t);
   int n = t.size(), m = p.size();
   std::vector<int> ret;
   for (int i = 0; i < n; ++i) {
-    if (v[m + 1 + i] == m) {
+    if (z[m + 1 + i] == m) {
       ret.push_back(i);
     }
   }
