0a8ff4ad27ed45a118f78c73a4da2a07c46c278f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-09-12 10:22:28 +0200

    update the compressor interface

--- compressor.h
+++ compressor.h
@@ -1,13 +1,13 @@
 #include <algorithm>
 #include <vector>
 
-class Compressor : public std::vector<int64_t> {
- public:
-  Compressor(const std::vector<int64_t>& v) : std::vector<int64_t>(v) {
-    std::sort(begin(), end());
-    erase(std::unique(begin(), end()), end());
+struct Compressor {
+  Compressor(const std::vector<int64_t>& v) : coord(v) {
+    std::sort(coord.begin(), coord.end());
+    coord.erase(std::unique(coord.begin(), coord.end()), coord.end());
   }
   int64_t operator()(int64_t x) const {
-    return std::lower_bound(begin(), end(), x) - begin();
+    return std::lower_bound(coord.begin(), coord.end(), x) - coord.begin();
   }
+  std::vector<int64_t> coord;
 };
--- compressor_test.cc
+++ compressor_test.cc
@@ -6,8 +6,8 @@ TEST(compressor, simple) {
   std::vector<int64_t> v = {-1, 2, 7, 2, 5};
   std::vector<int64_t> w = {-1, 2, 5, 7};
   Compressor c(v);
+  EXPECT_EQ(c.coord, w);
   for (size_t i = 0; i < w.size(); ++i) {
     EXPECT_EQ(c(w[i]), i);
-    EXPECT_EQ(c[i], w[i]);
   }
 }
