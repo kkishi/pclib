1b7c3a1197619184a6ba0781f09d493fec83cc61
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2021-12-12 14:12:57 +0100

    simplify

--- quotients.h
+++ quotients.h
@@ -7,16 +7,16 @@
 // for (int i = 1; i <= x; ++i) ++cnt[x / i];
 // return cnt;
 //
-// Note that the return value is not sorted in any way.
+// The return value is sorted in descending order.
+//
+// The algorithm is based on: https://youtu.be/ZrSO7fB7nlA?t=5034
 std::vector<std::pair<int64_t, int64_t>> Quotients(int64_t x) {
   std::vector<std::pair<int64_t, int64_t>> v;
-  for (int64_t d = 1; d * d <= x; ++d) {
-    int64_t q = x / d;
-    v.emplace_back(q, 1);
-    if (q > d) {
-      int64_t Q = x / (d + 1);
-      v.emplace_back(d, q - Q);
-    }
+  for (int64_t i = 1; i <= x;) {
+    int64_t q = x / i;
+    int64_t j = x / q + 1;
+    v.emplace_back(q, j - i);
+    i = j;
   }
   return v;
 }
