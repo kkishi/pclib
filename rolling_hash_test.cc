7f74db0243ec521452b7d560c631a412ba00f9b2
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-02-04 09:54:01 +0100

    fix rollinghash

--- rolling_hash_test.cc
+++ rolling_hash_test.cc
@@ -9,7 +9,7 @@ TEST(rollinghash, simple) {
   std::unordered_set<RollingHash> s;
   for (auto& str : std::vector<std::string>{"abc", "def", "ghi"}) {
     RollingHash h;
-    for (char c : str) h.Add(c);
+    for (char c : str) h.PushBack(c);
     s.insert(h);
   }
   EXPECT_EQ(s.size(), 3);
