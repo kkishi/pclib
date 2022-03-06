1d81881935b4ba47f69c0f815e3109ffaa6dcd25
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-03-06 16:46:32 +0100

    fix test

--- mo_test.cc
+++ mo_test.cc
@@ -5,35 +5,27 @@
 
 using testing::ElementsAre;
 
-struct DS {
-  DS(int n) : c(n), kinds(n), unique_kinds(0) {}
-  void Add(int i) {
+TEST(mo, abc174_sample2) {
+  std::vector<int> c = {2, 5, 6, 5, 2, 1, 7, 9, 7, 2};
+  std::vector<std::pair<int64_t, int64_t>> q = {{4, 5},  {1, 4}, {5, 7}, {1, 2},
+                                                {6, 8},  {6, 9}, {0, 8}, {5, 9},
+                                                {7, 10}, {5, 8}};
+
+  int n = c.size();
+  std::vector<int> kinds(n);
+  int unique_kinds = 0;
+
+  auto add = [&](int i) {
     if (kinds[c[i] - 1]++ == 0) {
       ++unique_kinds;
     }
-  }
-  void Del(int i) {
+  };
+  auto del = [&](int i) {
     if (--kinds[c[i] - 1] == 0) {
       --unique_kinds;
     }
-  }
-  int Get() const { return unique_kinds; }
-  std::vector<int> c;
-  std::vector<int> kinds;
-  int unique_kinds;
-};
+  };
+  auto get = [&]() { return unique_kinds; };
 
-TEST(mo, abc174_sample2) {
-  std::vector<int> c = {2, 5, 6, 5, 2, 1, 7, 9, 7, 2};
-  DS ds(c.size());
-  ds.c = c;
-
-  Mo<int, DS> mo(ds);
-
-  std::vector<std::pair<int, int>> q = {{5, 5},  {2, 4}, {6, 7}, {2, 2},
-                                        {7, 8},  {7, 9}, {1, 8}, {6, 9},
-                                        {8, 10}, {6, 8}};
-  for (auto [l, r] : q) mo.AddQuery(l - 1, r);
-
-  EXPECT_THAT(mo.ProcessQueries(), ElementsAre(1, 2, 2, 1, 2, 2, 6, 3, 3, 3));
+  EXPECT_THAT(Mo(add, del, get, q), ElementsAre(1, 2, 2, 1, 2, 2, 6, 3, 3, 3));
 }
