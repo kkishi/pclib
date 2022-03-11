111e00ef43cccb56f326e426f86829df3e66097f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-03-11 17:05:15 +0100

    add cumulative sum

---
+++ cumulative_sum.h
@@ -0,0 +1,58 @@
+#include <cassert>
+#include <vector>
+
+template <typename T>
+class CumulativeSum1D {
+ public:
+  CumulativeSum1D(const std::vector<T> &val) : sum_(val), n_(val.size()) {
+    for (int i = 1; i < n_; ++i) {
+      sum_[i] += sum_[i - 1];
+    }
+  }
+  // Returns sum(val(r)) for i<=r<=j.
+  T Get(int i, int j) const {
+    assert(i <= j);
+    return Get(j) - Get(i - 1);
+  }
+  // Returns sum(val(r)) for r<=i.
+  T Get(int i) const {
+    if (i < 0) return T();
+    return sum_[std::min(i, n_ - 1)];
+  }
+
+ private:
+  std::vector<T> sum_;
+  int n_;
+};
+
+template <typename T>
+class CumulativeSum2D {
+ public:
+  CumulativeSum2D(const std::vector<std::vector<T>> &val)
+      : sum_(val), n_(val.size()), m_(val[0].size()) {
+    for (int i = 0; i < n_; ++i) {
+      for (int j = 1; j < m_; ++j) {
+        sum_[i][j] += sum_[i][j - 1];
+      }
+    }
+    for (int i = 1; i < n_; ++i) {
+      for (int j = 0; j < m_; ++j) {
+        sum_[i][j] += sum_[i - 1][j];
+      }
+    }
+  }
+  // Returns sum(val(r, c)) for i<=r<=k, j<=c<=l.
+  T Get(int i, int j, int k, int l) const {
+    assert(i <= k && j <= l);
+    return Get(k, l) - Get(i - 1, l) - Get(k, j - 1) + Get(i - 1, j - 1);
+  }
+  // Returns sum(val(r, c)) for r<=i, c<=j.
+  T Get(int i, int j) const {
+    if (i < 0 || j < 0) return T();
+    return sum_[std::min(i, n_ - 1)][std::min(j, m_ - 1)];
+  }
+
+ private:
+  std::vector<std::vector<T>> sum_;
+  int n_, m_;
+};
