2bee5e2d662a8f9917c24e9d9e7e9c775fb10b16
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-03-25 10:30:51 +0100

    improve MinCostFlow

--- min_cost_flow.h
+++ min_cost_flow.h
@@ -7,7 +7,6 @@ class MinCostFlow {
  public:
   void Add(int from, int to, Cap cap, Cost cost, Cap min_flow = 0) {
     assert(0 <= min_flow && min_flow <= cap);
-    assert(cost >= 0 || min_flow == 0);  // Not supported yet.
     edges_.emplace_back(from, to, cap, cost, min_flow);
     n_ = std::max(n_, std::max(from, to) + 1);
   }
@@ -17,16 +16,20 @@ class MinCostFlow {
     g.add_edge(t, s, std::numeric_limits<Cap>::max(), 0);
     Cost sum = 0;
     for (auto [from, to, cap, cost, min_flow] : edges_) {
+      auto excess = [&](int i, Cap cap) {
+        if (cap > 0) g.add_edge(S, i, cap, 0);
+        if (cap < 0) g.add_edge(i, T, -cap, 0);
+      };
+      if (min_flow > 0) {
+        cap -= min_flow;
+        excess(from, -min_flow);
+        excess(to, min_flow);
+      }
       if (cost < 0) {
         sum += cap * cost;
         g.add_edge(to, from, cap, -cost);
-        g.add_edge(S, to, cap, 0);
-        g.add_edge(from, T, cap, 0);
-      } else if (min_flow > 0) {
-        assert(min_flow <= cap);
-        g.add_edge(from, to, cap - min_flow, cost);
-        g.add_edge(S, to, min_flow, 0);
-        g.add_edge(from, T, min_flow, 0);
+        excess(from, -cap);
+        excess(to, cap);
       } else {
         g.add_edge(from, to, cap, cost);
       }
@@ -37,6 +40,6 @@ class MinCostFlow {
   }
 
  private:
-  int n_ = 0;
+  int n_ = 0;  // The number of nodes in the original graph.
   std::vector<std::tuple<int, int, Cap, Cost, Cap>> edges_;
 };
