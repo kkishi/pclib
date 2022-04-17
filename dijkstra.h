b8f14a90e297e4c26767b0c25cd6be05e12094e3
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-04-17 23:57:03 +0200

    update dijkstra

--- dijkstra.h
+++ dijkstra.h
@@ -11,10 +11,10 @@ struct Result {
   std::vector<std::optional<T>> dist;
   std::vector<std::optional<int64_t>> prev;
   std::vector<int64_t> Path(int64_t dest) const {
-    dassert(prev[dest]);
     std::vector<int64_t> v;
-    while (dest >= 0) {
+    while (true) {
       v.push_back(dest);
+      if (!prev[dest]) break;
       dest = *prev[dest];
     }
     std::reverse(v.begin(), v.end());
@@ -23,32 +23,32 @@ struct Result {
 };
 
 template <typename T>
-struct DijkstraState {
-  T dist;
-  int64_t node;
-  int64_t prev;
-  bool operator<(const DijkstraState& s) const { return dist > s.dist; }
-};
-
-template <typename T>
 Result<T> Dijkstra(const WeightedGraph<T>& graph, int64_t source) {
   const int32_t n = graph.size();
 
   std::vector<std::optional<T>> dist(n);
   std::vector<std::optional<int64_t>> prev(n);
+  dist[source] = 0;
 
-  std::priority_queue<DijkstraState<T>> que;
-  que.push({0, source, -1});
+  struct S {
+    T dist;
+    int64_t node;
+    bool operator<(const S& s) const { return dist > s.dist; }
+  };
+  std::priority_queue<S> que;
+  que.push({0, source});
 
   while (!que.empty()) {
-    auto [d, n, p] = que.top();
+    auto [d, n] = que.top();
     que.pop();
-    if (dist[n]) continue;
-    dist[n] = d;
-    prev[n] = p;
+    if (d > *dist[n]) continue;
     for (auto [c, w] : graph[n]) {
       dassert(w >= 0);
-      que.push({d + w, c, n});
+      if (!dist[c] || *dist[c] > d + w) {
+        dist[c] = d + w;
+        prev[c] = n;
+        que.push({d + w, c});
+      }
     }
   }
   return {dist, prev};
--- dijkstra_test.cc
+++ dijkstra_test.cc
@@ -2,8 +2,11 @@
 
 #include <vector>
 
+#include "gmock/gmock.h"
 #include "graph.h"
 #include "gtest/gtest.h"
+
+using ::testing::ElementsAre;
 
 TEST(dijkstra, simple) {
   WeightedGraph<int> graph = {{{2, 3}, {1, 1}}, {{2, 1}}, {}};
@@ -11,6 +14,5 @@ TEST(dijkstra, simple) {
   Result<int> res = Dijkstra(graph, 0);
   EXPECT_TRUE(res.dist[2]);
   EXPECT_EQ(*res.dist[2], 2);
-
-  EXPECT_EQ(res.Path(2), (std::vector<int32_t>{0, 1, 2}));
+  EXPECT_THAT(res.Path(2), ElementsAre(0, 1, 2));
 }
