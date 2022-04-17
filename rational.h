0f922743beb54138e9c93cae7e66b84e00d0343d
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-04-17 23:33:40 +0200

    update rational

--- rational.h
+++ rational.h
@@ -1,41 +1,37 @@
+#include <cstdint>
 #include <numeric>
 #include <ostream>
+#include <utility>
 
-#include "dassert.h"
-
-template <typename T>
-class Rational {
+class Rational : public std::pair<int64_t, int64_t> {
  public:
-  Rational(T p, T q) {
-    dassert(q != 0);
+  using std::pair<int64_t, int64_t>::pair;  // Inherit constructors.
+  void Normalize() {
+    auto& [p, q] = *this;
+    if (q == 0) {
+      p = 0;
+      return;
+    }
     if (p == 0) {
-      p_ = 0;
-      q_ = 1;
+      q = 1;
       return;
     }
     if (q < 0) {
       p = -p;
       q = -q;
     }
-    T sign = 1;
-    if (p < 0) {
-      sign = -1;
-      p = -p;
-    }
-    T g = std::gcd(p, q);
-    p_ = sign * (p / g);
-    q_ = q / g;
+    int64_t g = std::gcd(p, q);
+    p /= g;
+    q /= g;
   }
-  T p() const { return p_; }
-  T q() const { return q_; }
-
- private:
-  T p_;
-  T q_;
+  static Rational Normalized(int64_t p, int64_t q) {
+    Rational r = {p, q};
+    r.Normalize();
+    return r;
+  }
 };
 
-template <typename T>
-std::ostream& operator<<(std::ostream& out, const Rational<T>& r) {
-  out << r.p() << '/' << r.q();
+std::ostream& operator<<(std::ostream& out, const Rational& r) {
+  out << r.first << '/' << r.second;
   return out;
 }
--- rational_test.cc
+++ rational_test.cc
@@ -5,32 +5,36 @@
 #include "gtest/gtest.h"
 
 TEST(rational, simple) {
-  Rational<int> rat(1, 2);
-  EXPECT_EQ(rat.p(), 1);
-  EXPECT_EQ(rat.q(), 2);
+  Rational rat(1, 2);
+  rat.Normalize();
+  EXPECT_EQ(rat.first, 1);
+  EXPECT_EQ(rat.second, 2);
 }
 
 TEST(rational, normalization) {
-  Rational<int> rat(2 * 3 * 5, -3 * 5 * 7);
-  EXPECT_EQ(rat.p(), -2);
-  EXPECT_EQ(rat.q(), 7);
+  Rational rat(2 * 3 * 5, -3 * 5 * 7);
+  rat.Normalize();
+  EXPECT_EQ(rat.first, -2);
+  EXPECT_EQ(rat.second, 7);
 }
 
 TEST(rational, normalization_negative) {
-  Rational<int> rat(-3, -7);
-  EXPECT_EQ(rat.p(), 3);
-  EXPECT_EQ(rat.q(), 7);
+  Rational rat(-3, -7);
+  rat.Normalize();
+  EXPECT_EQ(rat.first, 3);
+  EXPECT_EQ(rat.second, 7);
 }
 
 TEST(rational, zero) {
-  Rational<int> rat(0, -42);
-  EXPECT_EQ(rat.p(), 0);
-  EXPECT_EQ(rat.q(), 1);
+  Rational rat(0, -42);
+  rat.Normalize();
+  EXPECT_EQ(rat.first, 0);
+  EXPECT_EQ(rat.second, 1);
 }
 
 TEST(rational, stream) {
   std::ostringstream oss;
-  Rational<int> rat(-3, 7);
+  Rational rat(-3, 7);
   oss << rat;
   EXPECT_EQ(oss.str(), "-3/7");
 }
