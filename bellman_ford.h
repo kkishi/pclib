12a01ad83c73d806b742a5efff3e56f281835e2f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-08-13 16:10:59 +0200

    update bellman ford to explicitly handle unreachable state

--- bellman_ford.h
+++ bellman_ford.h
@@ -4,19 +4,18 @@
 #include "graph.h"
 
 template <typename T>
-std::pair<std::vector<T>, bool> BellmanFord(const WeightedGraph<T>& graph,
-                                            int start) {
+std::pair<std::vector<std::optional<T>>, bool> BellmanFord(
+    const WeightedGraph<T>& graph, int start) {
   const int n = graph.size();
 
-  T inf = std::numeric_limits<T>::max();
-  std::vector<T> dist(n, inf);
+  std::vector<std::optional<T>> dist(n);
   dist[start] = 0;
 
   for (int iter = 0; iter < n; ++iter) {
     for (int i = 0; i < n; ++i) {
-      if (dist[i] == inf) continue;
+      if (!dist[i]) continue;
       for (auto [j, w] : graph[i]) {
-        if (T d = dist[i] + w; dist[j] > d) {
+        if (T d = *dist[i] + w; !dist[j] || *dist[j] > d) {
           if (iter == n - 1) return {dist, false};
           dist[j] = d;
         }
