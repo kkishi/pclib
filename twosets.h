7666272c01fc3a56acf4f4117f8ea0f7f50e15af
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2022-12-17 06:55:09 +0100

    update twosets

--- twosets.h
+++ twosets.h
@@ -3,37 +3,58 @@
 
 #include "dassert.h"
 
+class SetWithSum {
+  using i64 = int64_t;
+
+ public:
+  void Add(i64 x) {
+    set_.insert(x);
+    sum_ += x;
+  }
+  bool Rem(i64 x) {
+    auto it = set_.find(x);
+    if (it == set_.end()) return false;
+    sum_ -= x;
+    set_.erase(it);
+    return true;
+  }
+  const std::multiset<i64>& set() const { return set_; }
+  i64 sum() const { return sum_; }
+
+  std::multiset<i64> set_;
+  i64 sum_ = 0;
+};
+
 class TwoSets {
   using i64 = int64_t;
 
  public:
   void Add(i64 x) {
-    l_.insert(x);
-    ls_ += x;
+    if (h_.set().empty() || x < *h_.set().begin()) {
+      l_.Add(x);
+    } else {
+      h_.Add(x);
+    }
   }
-  i64 Size() const { return l_.size() + h_.size(); }
+  void Rem(i64 x) { l_.Rem(x) || h_.Rem(x); }
+  i64 Size() const { return l_.set().size() + h_.set().size(); }
   void Adjust(i64 k) {
     dassert(k <= Size());
-    while (i64(l_.size()) < k) {
-      auto it = h_.begin();
-      l_.insert(*it);
-      ls_ += *it;
-      hs_ -= *it;
-      h_.erase(it);
+    while (i64(l_.set().size()) < k) {
+      int x = *h_.set().begin();
+      l_.Add(x);
+      h_.Rem(x);
     }
-    while (i64(l_.size()) > k) {
-      auto it = prev(l_.end());
-      h_.insert(*it);
-      hs_ += *it;
-      ls_ -= *it;
-      l_.erase(it);
+    while (i64(l_.set().size()) > k) {
+      int x = *prev(l_.set().end());
+      h_.Add(x);
+      l_.Rem(x);
     }
   }
-  i64 Sum() const { return ls_ + hs_; }
-  i64 LSum() const { return ls_; }
-  i64 HSum() const { return hs_; }
+  i64 Sum() const { return LSum() + HSum(); }
+  i64 LSum() const { return l_.sum(); }
+  i64 HSum() const { return h_.sum(); }
 
  private:
-  std::multiset<i64> l_, h_;
-  i64 ls_ = 0, hs_ = 0;
+  SetWithSum l_, h_;
 };
