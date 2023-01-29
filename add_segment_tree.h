b89eaac28ddc91325244e6a3993b83613cd9cf82
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-01-29 11:12:22 +0100

    add utility to add_segment_tree

--- add_segment_tree.h
+++ add_segment_tree.h
@@ -4,4 +4,5 @@ template <typename T>
 class AddSegmentTree : public SegmentTree<T> {
  public:
   AddSegmentTree(int n) : SegmentTree<T>(n, [](T a, T b) { return a + b; }) {}
+  void Add(int i, T v) { this->Set(i, this->Get(i) + v); }
 };
