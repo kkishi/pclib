1d546a6a0578df755678d34332b91bd63b8e5bd8
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-03-08 20:35:13 +0100

    update Convex Hull Trick

--- convex_hull_trick.h
+++ convex_hull_trick.h
@@ -4,50 +4,46 @@
 #include "binary_search.h"
 #include "dassert.h"
 
+template <typename T, typename Cmp>
 class ConvexHullTrick {
-  using i64 = int64_t;
-
  public:
-  void Add(i64 a, i64 b) {
+  void Add(T a, T b) {
     auto [it, ok] = mp_.insert({a, b});
-    if (!ok && it->second >= b) return;
-    auto necessary = [&](auto it) -> bool {
-      if (it == mp_.begin()) return true;
+    if (!ok && cmp_(b, it->second)) return;
+    auto erase = [&](auto it) -> bool {
+      if (it == mp_.begin()) return false;
       auto [al, bl] = *prev(it);
 
       auto jt = next(it);
-      if (jt == mp_.end()) return true;
+      if (jt == mp_.end()) return false;
       auto [ar, br] = *jt;
 
       auto [am, bm] = *it;
-      return (am - al) * (bl - br) > (bl - bm) * (ar - al);
-    };
-    if (!necessary(it)) {
+      if (cmp_((bl - bm) * (ar - al), (am - al) * (bl - br))) return false;
       mp_.erase(it);
-      return;
-    }
+      return true;
+    };
+    if (erase(it)) return;
     for (auto jt = it; jt != mp_.begin();) {
       --jt;
-      if (necessary(jt)) break;
       auto kt = prev(jt);
-      mp_.erase(jt);
+      if (!erase(jt)) break;
       jt = kt;
     }
     for (auto jt = next(it); jt != mp_.end();) {
-      if (necessary(jt)) break;
       auto kt = next(jt);
-      mp_.erase(jt);
+      if (!erase(jt)) break;
       jt = kt;
     }
   }
-  i64 Get(i64 x) const {
+  T Get(T x) const {
     dassert(!mp_.empty());
-    i64 a = BinarySearch<i64>(
-        mp_.begin()->first, prev(mp_.end())->first + 1, [&](i64 a) -> bool {
+    T a = BinarySearch<T>(
+        mp_.begin()->first, prev(mp_.end())->first + 1, [&](T a) -> bool {
           auto it = mp_.lower_bound(a);
           if (it == mp_.begin()) return true;
           auto jt = prev(it);
-          return it->first * x + it->second > jt->first * x + jt->second;
+          return cmp_(jt->first * x + jt->second, it->first * x + it->second);
         });
     auto it = mp_.find(a);
     dassert(it != mp_.end() && it->first == a);
@@ -55,5 +51,9 @@ class ConvexHullTrick {
   }
 
  private:
-  std::map<i64, i64> mp_;
+  std::map<T, T> mp_;
+  static const Cmp cmp_;
 };
+
+using MaxConvexHullTrick = ConvexHullTrick<int64_t, std::less<int64_t>>;
+using MinConvexHullTrick = ConvexHullTrick<int64_t, std::greater<int64_t>>;
