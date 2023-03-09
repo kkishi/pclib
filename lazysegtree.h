45732f70d4c99f5854000f8726439106cec170fd
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-03-09 09:55:23 +0100

    add add_max::segtree

--- lazysegtree.h
+++ lazysegtree.h
@@ -23,6 +23,17 @@ F id() { return 0; }
 using segtree = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
 }  // namespace add_min
 
+namespace add_max {
+using S = int64_t;
+S op(S a, S b) { return std::max(a, b); }
+S e() { return std::numeric_limits<S>::min(); }
+using F = int64_t;
+S mapping(F f, S x) { return f + x; }
+F composition(F f, F g) { return f + g; }
+F id() { return 0; }
+using segtree = atcoder::lazy_segtree<S, op, e, F, mapping, composition, id>;
+}  // namespace add_max
+
 namespace update_sum {
 struct S {
   int64_t val;
