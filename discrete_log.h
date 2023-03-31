f649c17986bccd0032daa1983e359831b9e42879
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-04-01 00:54:38 +0200

    update

--- discrete_log.h
+++ discrete_log.h
@@ -4,5 +4,5 @@
 // -1 if there's no such n.
 int64_t DiscreteLog(int64_t x, int64_t y, int64_t m) {
   auto f = [&](int64_t a, int64_t b) { return a * b % m; };
-  return BSGS<int64_t, int64_t>(x, 1, y, m, f, f);
+  return BSGS<int64_t, int64_t>(x, 1 % m, y, m, f, f);
 }
