5550dff617428768996dfa3cd9b453140cc0e74f
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-04-05 13:34:04 +0200

    add convex contains

--- int_geometry.h
+++ int_geometry.h
@@ -2,31 +2,72 @@
 #define INT_GEOMETRY_H_
 
 #include <complex>
+#include <vector>
 
-class Point : public std::complex<int> {
-  using std::complex<int>::complex;  // Inherit constructors.
- public:
-  int x() const { return real(); }
-  int y() const { return imag(); }
+#include "binary_search.h"
+
+struct Point {
+  int64_t x, y;
 };
 
+Point& operator-=(Point& a, Point b) {
+  a.x -= b.x;
+  a.y -= b.y;
+  return a;
+}
+
+Point operator-(Point a, Point b) { return a -= b; }
+
+Point& operator+=(Point& a, Point b) {
+  a.x += b.x;
+  a.y += b.y;
+  return a;
+}
+
+Point operator+(Point a, Point b) { return a += b; }
+
+bool operator==(const Point& a, const Point& b) {
+  return a.x == b.x && a.y == b.y;
+}
+
 std::istream& operator>>(std::istream& is, Point& p) {
-  int x, y;
-  is >> x >> y;
-  p = {x, y};
+  is >> p.x >> p.y;
   return is;
 }
 
+namespace std {
+std::ostream& operator<<(std::ostream& os, Point p) {
+  os << "(" << p.x << "," << p.y << ")";
+  return os;
+}
+}  // namespace std
+
+int64_t Cross(Point a, Point b) { return a.x * b.y - b.x * a.y; }
+int64_t Dot(Point a, Point b) { return a.x * b.x + a.y * b.y; }
+int64_t Norm(Point p) { return Dot(p, p); }
+
+int CCW(Point a, Point b, Point c) {
+  b -= a;
+  c -= a;
+  if (Cross(b, c) > 0) return 1;
+  if (Cross(b, c) < 0) return -1;
+  if (Dot(b, c) < 0) return 2;
+  if (Norm(b) < Norm(c)) return -2;
+  return 0;
+}
+
+Point Rot90(Point p) { return {-p.y, p.x}; }
+
 struct Circle {
   Point c;
-  int r;
+  int64_t r;
 };
 
 bool Intersects(const Circle& a, const Circle& b) {
-  auto sq = [](int x) { return x * x; };
+  auto sq = [](int64_t x) { return x * x; };
 
   // Is a outside of b without an intersection?
-  int d2 = norm(b.c - a.c);
+  int64_t d2 = Norm(b.c - a.c);
   if (d2 > sq(a.r + b.r)) return false;
 
   // Does a contain b without an intersection?
@@ -36,20 +77,19 @@ bool Intersects(const Circle& a, const Circle& b) {
   return !contains(a, b) && !contains(b, a);
 }
 
-int Cross(Point a, Point b) { return a.x() * b.y() - b.x() * a.y(); }
+using Polygon = std::vector<Point>;
 
-int Dot(Point a, Point b) { return a.x() * b.x() + a.y() + b.y(); }
-
-int CCW(Point a, Point b, Point c) {
-  b -= a;
-  c -= a;
-  if (Cross(b, c) > 0) return 1;
-  if (Cross(b, c) < 0) return -1;
-  if (Dot(b, c) < 0) return 2;
-  if (Dot(b, b) < Dot(c, c)) return -2;
-  return 0;
+int Contains(const Polygon& P, Point p) {
+  int n = P.size();
+  auto lof = [&](Point a, Point b) { return Cross(b - a, p - a) > 0; };
+  auto rof = [&](Point a, Point b) { return Cross(b - a, p - a) < 0; };
+  if (rof(P[0], P[1])) return 0;
+  if (lof(P[0], P[n - 1])) return 0;
+  int i = BinarySearch<int>(n - 1, 1, [&](int i) { return !lof(P[0], P[i]); });
+  if (rof(P[i - 1], P[i])) return 0;
+  auto on = [&](Point a, Point b) { return CCW(a, b, p) == 0; };
+  if (on(P[0], P[1]) || on(P[i - 1], P[i]) || on(P[0], P[n - 1])) return 2;
+  return 1;
 }
-
-Point Rot90(Point p) { return {-p.y(), p.x()}; }
 
 #endif  // INT_GEOMETRY_H_
