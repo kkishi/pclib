e4a04c1d41eca2b8e6c70a00e73cfa4a69290675
Keisuke Kishimoto <keisuke.kishimoto@gmail.com>
2023-12-20 02:38:53 +0100

    update dual_segment_tree to respect application order

--- dual_segment_tree.h
+++ dual_segment_tree.h
@@ -14,34 +14,46 @@ class DualSegmentTree {
     v_.resize(two * 2 - 1, identity_);
   }
   void Update(int begin, int end, T v) {
+    Eval(begin);
+    Eval(end - 1);
     int l = Leaf(begin), r = Leaf(end);
     while (l < r) {
       if (IsRight(l)) {
-        v_[l] = operation_(v_[l], v);
+        v_[l] = operation_(v, v_[l]);
         ++l;
       }
       l = Parent(l);
       if (IsRight(r)) {
-        v_[r - 1] = operation_(v_[r - 1], v);
+        v_[r - 1] = operation_(v, v_[r - 1]);
       }
       r = Parent(r);
     }
   }
-  T Get(int i) const {
-    T v = identity_;
-    int index = Leaf(i);
-    while (true) {
-      v = operation_(v, v_[index]);
-      if (index == 0) break;
-      index = Parent(index);
-    }
-    return v;
+  T Get(int i) {
+    Eval(i);
+    return v_[Leaf(i)];
   }
 
  private:
+  void Eval(int i) {
+    std::vector<int> v;
+    int index = Leaf(i);
+    while (index != 0) {
+      index = Parent(index);
+      v.push_back(index);
+    }
+    for (size_t i = 0; i < v.size(); ++i) {
+      int index = v[v.size() - 1 - i];
+      v_[Left(index)] = operation_(v_[index], v_[Left(index)]);
+      v_[Right(index)] = operation_(v_[index], v_[Right(index)]);
+      v_[index] = identity_;
+    }
+  }
   int Leaf(int i) const { return i + (v_.size() >> 1); }
   bool IsRight(int i) const { return !(i & 1); }
   int Parent(int i) const { return (i - 1) >> 1; }
+  int Left(int i) const { return (i << 1) + 1; }
+  int Right(int i) const { return (i << 1) + 2; }
   const Operation operation_;
   const T identity_;
   std::vector<T> v_;
--- dual_segment_tree_test.cc
+++ dual_segment_tree_test.cc
@@ -13,3 +13,12 @@ TEST(dual_segment_tree, simple) {
   EXPECT_EQ(t.Get(2), 3);
   EXPECT_EQ(t.Get(3), 2);
 }
+
+TEST(dual_segment_tree, application_order) {
+  using std::string;
+  auto concat = [](string f, string g) { return g + f; };
+  DualSegmentTree<string> t(8, concat);
+  string s = "abcdefgh";
+  for (int i = 0; i < 8; ++i) t.Update(i, 8, string(1, s[i]));
+  for (int i = 0; i < 8; ++i) EXPECT_EQ(t.Get(i), s.substr(0, i + 1));
+}
